c03ff2958f71c7990ca67915e8273da23a01527c
Alexis Zini <113997553+AlexisZnu@users.noreply.github.com>
2023-05-30 15:25:13 -0300

    Update ShopSteps.java

--- src/main/java/lippia/web/steps/ShopSteps.java
+++ src/main/java/lippia/web/steps/ShopSteps.java
@@ -2,7 +2,6 @@ package lippia.web.steps;
 
 import com.crowdar.core.PageSteps;
 import io.cucumber.java.en.*;
-import lippia.web.services.HomePageServices;
 import lippia.web.services.ShopService;
 import lippia.web.services.SuperiorBarNavigationBarService;
 
